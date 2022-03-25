e4e25cc7f48e266b9316f27caeb8d356ade59953
nobkz <tone.nobukazu@gmail.com>
2022-03-25 18:15:12 +0900

    マルとバツが交互に切り変えるようにした

--- main.js
+++ main.js
@@ -51,13 +51,20 @@ const x1s = xs.map((x) => margin + x * wl);
 const x2s = xs.map((x) => margin + (x + 1) * wl );
 const y1s = ys.map((y) => margin + y * hl);
 const y2s = ys.map((y) => margin + (y + 1) * hl );
+let maru = true;
 
 canvas.addEventListener("click", (e) => {
     console.log(`x: ${e.clientX} y: ${e.clientY}`);
     xs.forEach((x, index) => {
         ys.forEach((y, index2) => {
             if (e.clientX < x2s[index] && e.clientX > x1s[index] && e.clientY < y2s[index2] && e.clientY > y1s[index2]) {
-                check("batsu", x - 1, y -1);
+                if(maru){
+                    check("maru", x - 1, y -1);
+                    maru = false;
+                }else{
+                    check("batsu", x - 1, y -1);
+                    maru = true;
+                }
             }
         });
     });
